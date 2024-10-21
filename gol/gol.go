09add718a966747a1b171967f6e67121d77fd98f
moustafa <cw22322@bristol.ac.uk>
2024-10-21 20:32:22 +0100

    added channels

--- gol/gol.go
+++ gol/gol.go
@@ -13,15 +13,19 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
 	//	TODO: Put the missing channels in here.
 
+	filename := make(chan string)
+	output := make(chan uint8)
+	input := make(chan uint8)
+
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: filename,
+		output:   output,
+		input:    input,
 	}
 	go startIo(p, ioChannels)
 
@@ -29,9 +33,9 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 		events:     events,
 		ioCommand:  ioCommand,
 		ioIdle:     ioIdle,
-		ioFilename: nil,
-		ioOutput:   nil,
-		ioInput:    nil,
+		ioFilename: filename,
+		ioOutput:   output,
+		ioInput:    input,
 	}
 	distributor(p, distributorChannels)
 }
