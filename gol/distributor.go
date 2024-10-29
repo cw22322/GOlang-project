d8694886f93c6d8426df6451fa919d200a846801
Moustafa <cw22322@bristol.ac.uk>
2024-10-29 15:52:47 +0000

    Added single threaded

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,10 @@
 package gol
 
+import (
+	"strconv"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,24 +14,125 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	var alivecells []util.Cell
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] != 0 {
+				alivecells = append(alivecells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	return alivecells
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+
+	for y := 0; y < len(world); y++ {
+		for x := 0; x < len(world[0]); x++ {
+			alive := 0
+
+			for dy := -1; dy <= 1; dy++ {
+				for dx := -1; dx <= 1; dx++ {
+					if dy == 0 && dx == 0 {
+						continue
+					}
+
+					nx, ny := dx+x, dy+y
+
+					if nx < 0 {
+						nx = len(world[0]) - 1
+					}
+
+					if ny < 0 {
+						ny = len(world) - 1
+					}
+
+					if nx >= len(world[0]) {
+						nx = 0
+					}
+
+					if ny >= len(world) {
+						ny = 0
+					}
+
+					if world[ny][nx] == 255 {
+						alive++
+					}
+
+				}
+
+			}
+			if world[y][x] == 255 {
+				if alive < 2 {
+					newWorld[y][x] = 0
+				} else if alive == 2 || alive == 3 {
+					newWorld[y][x] = 255
+				} else {
+					newWorld[y][x] = 0
+				}
+			} else {
+				if alive == 3 {
+					newWorld[y][x] = 255
+
+				} else {
+					newWorld[y][x] = 0
+				}
+			}
+		}
+	}
+
+	return newWorld
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
+	c.ioCommand <- ioInput
+	filename := strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight)
+	c.ioFilename <- filename
+
+	World := make([][]uint8, p.ImageHeight)
+	for i := range World {
+		World[i] = make([]uint8, p.ImageWidth)
+		for j := 0; j < p.ImageWidth; j++ {
+		}
+	}
+
+	for i := range World {
+		for j := 0; j < p.ImageWidth; j++ {
+			World[i][j] = <-c.ioInput
+		}
+	}
 
 	// TODO: Create a 2D slice to store the world.
-
 	turn := 0
 	c.events <- StateChange{turn, Executing}
 
 	// TODO: Execute all turns of the Game of Life.
 
+	for turn = 0; turn < p.Turns; turn++ {
+		World = calculateNextState(p, World)
+	}
+	alive := calculateAliveCells(p, World)
+
 	// TODO: Report the final state using FinalTurnCompleteEvent.
+
+	Finalturn := FinalTurnComplete{
+		CompletedTurns: turn,
+		Alive:          alive,
+	}
+
+	c.events <- Finalturn
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
-	<-c.ioIdle
+	//false <- c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }
