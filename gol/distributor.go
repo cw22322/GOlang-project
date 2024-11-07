d3bb1024074230072bbfe31445e38a23aaf36cac
stoyx <cw22322@bristol.ac.uk>
2024-11-07 23:47:08 +0000

    balls

--- gol/distributor.go
+++ gol/distributor.go
@@ -32,6 +32,7 @@ type Response struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
+	var turn int
 	c.ioCommand <- ioInput
 	filename := strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight)
 	c.ioFilename <- filename
@@ -58,14 +59,58 @@ func distributor(p Params, c distributorChannels) {
 	}
 
 	done := make(chan struct{})
+	paused := false
+	quitting := false
+	stateChan := make(chan State, 1)
+
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
 
 	var mu sync.Mutex
 	go func() {
 		var response Response
-		ticker := time.NewTicker(2 * time.Second)
-		defer ticker.Stop()
 		for {
 			select {
+			case key := <-c.KeyPresses:
+				switch key {
+				case 'p':
+					mu.Lock()
+					paused = true
+					if paused {
+						c.events <- StateChange{turn, Paused}
+					} else {
+						stateChan <- Executing
+						c.events <- StateChange{turn, Executing}
+					}
+					mu.Unlock()
+				case 's':
+					mu.Lock()
+					c.ioCommand <- ioCheckIdle
+					<-c.ioIdle
+
+					c.ioCommand <- ioOutput
+					filename = filename + "x" + strconv.Itoa(p.Turns)
+					c.ioFilename <- filename
+
+					client.Call("GameOfLife.Save", request, &response)
+					worldSend := response.LastWorld
+					for y := 0; y < len(response.LastWorld); y++ {
+						for x := 0; x < len(worldSend[0]); x++ {
+							c.ioOutput <- World[y][x]
+						}
+					}
+					c.ioCommand <- ioCheckIdle
+					<-c.ioIdle
+					mu.Unlock()
+					c.events <- ImageOutputComplete{CompletedTurns: turn, Filename: filename}
+				case 'q':
+					mu.Lock()
+					quitting = true
+					c.ioCommand <- ioCheckIdle
+					<-c.ioIdle
+					stateChan <- Quitting
+					mu.Unlock()
+				}
 			case <-ticker.C:
 				mu.Lock()
 				client.Call("GameOfLife.SendAlive", request, &response)
@@ -83,6 +128,10 @@ func distributor(p Params, c distributorChannels) {
 	close(done)
 	World = response.LastWorld
 	alive := response.AliveCells
+	turn = response.Turns
+
+	ticker.Stop()
+
 	c.events <- FinalTurnComplete{CompletedTurns: response.Turns, Alive: alive}
 
 	c.ioCommand <- ioOutput
